4150f490eb3767955dd197754d6a3bf68990b321
Vinícius <viniciusmpacheco22@gmail.com>
2024-03-27 12:40:43 -0300

    refac - invalid use of separators

--- src/main/java/StringCalculator.java
+++ src/main/java/StringCalculator.java
@@ -1,14 +1,24 @@
+import utils.RegularExpressionMatch;
+
 import static java.util.Arrays.stream;
+import static utils.RegularExpressionMatch.getValueSubstringFromRegularExpression;
 
 public class StringCalculator {
     public double add(String input){
-        if(input.contains("\n,") || input.contains(",\n")){
-            throw new IllegalArgumentException("Damn");
-        }
+        throwExceptionIfIllegalUseOfSeparators(input);
 
         return stream(input.split("[,\\n]"))
                 .filter(value -> !value.isBlank())
                 .mapToDouble(Double::parseDouble)
                 .sum();
+    }
+
+    private void throwExceptionIfIllegalUseOfSeparators(String input) {
+        String illegalSubstring = getValueSubstringFromRegularExpression(input, "(\\n,)|(,\\n)");
+        if(!illegalSubstring.isBlank()){
+            int indexOfIllegalSeparator = input.indexOf(illegalSubstring);
+            throw new IllegalArgumentException("Number expected but '\\n' found at position "
+                    + indexOfIllegalSeparator + ".");
+        }
     }
 }
---
+++ src/main/java/utils/RegularExpressionMatch.java
@@ -0,0 +1,18 @@
+package utils;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class RegularExpressionMatch {
+    private static final String EMPTY_STRING = "";
+    public static String getValueSubstringFromRegularExpression(String value, String regex){
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(value);
+
+        if(matcher.find()){
+            return matcher.group(0);
+        } else {
+            return EMPTY_STRING;
+        }
+    }
+}
